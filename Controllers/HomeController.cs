326149d67553f35078db7009dac0ebc0ba495d2c
buibup <mongkolm@hatari.net>
2019-02-27 16:57:26 +0700

    completed Creating Interactive Dashboards in Asp.net Core MVC

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,23 @@
+using AdminPanelTutorial.Models;
+using AdminPanelTutorial.ViewModels;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 namespace AdminPanelTutorial.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public IActionResult Index()
         {
-            return View();
+            DashboardViewModel dashboard = new DashboardViewModel();
+
+            dashboard.doctors_count = db.Doctors.Count();
+            dashboard.nurses_count = db.Nurses.Count();
+            dashboard.patients_count = db.Patients.Count();
+            return View(dashboard);
         }
     }
 }
